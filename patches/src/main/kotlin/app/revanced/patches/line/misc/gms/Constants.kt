7c45721bdfa1716916ae358c27633bea83931618
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-02-24 17:22:14 +0900

    Create Constants.kt

---
+++ patches/src/main/kotlin/app/revanced/patches/line/misc/gms/Constants.kt
@@ -0,0 +1,6 @@
+package app.revanced.patches.linr.misc.gms
+
+internal object Constants {
+    const val YOUTUBE_PACKAGE_NAME = "jp.naver.line.android"
+    const val REVANCED_YOUTUBE_PACKAGE_NAME = "app.revanced.jp.naver.line.android"
+}

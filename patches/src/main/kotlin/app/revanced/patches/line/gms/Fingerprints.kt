e9ab29c2b678293b24f40f71067726fad3ccab52
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-09 18:40:09 +0900

    Update Fingerprints.kt

--- patches/src/main/kotlin/app/revanced/patches/line/gms/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/line/gms/Fingerprints.kt
@@ -1,11 +1,19 @@
 package app.revanced.patches.line.gms.fingerprints
-import app.revanced.patcher.fingerprint
-object MainActivityFingerprint : fingerprint(
-    className = "Ljp/naver/line/android/activity/main/MainActivity;",
-    method = "onCreate",
-)
 
-object SafetyNetClientFingerprint : fingerprint(
-    className = "Lcom/google/android/gms/safetynet/SafetyNetClient;",
-    method = "attest"
-)
+import app.revanced.patcher.fingerprint
+
+internal val MainActivityFingerprint = fingerprint {
+    custom { methodDef, classDef ->
+        methodDef.name == "onCreate" &&
+        classDef.type == "Ljp/naver/line/android/activity/main/MainActivity;" &&
+        methodDef.parameterTypes == listOf("Landroid/os/Bundle;")
+    }
+}
+
+internal val SafetyNetClientFingerprint = fingerprint {
+    custom { methodDef, classDef ->
+        methodDef.name == "attest" &&
+        classDef.type == "Lcom/google/android/gms/safetynet/SafetyNetClient;" &&
+        methodDef.returnType == "Lcom/google/android/gms/tasks/Task;"
+    }
+}

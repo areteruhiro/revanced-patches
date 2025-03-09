85621978e748f2dc28b91e0251d1d02d617bebf4
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-09 18:43:20 +0900

    Update ExtensionPatch.kt

--- patches/src/main/kotlin/app/revanced/patches/line/misc/ExtensionPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/line/misc/ExtensionPatch.kt
@@ -1,25 +1,5 @@
 package app.revanced.patches.line.misc.extension
 
-import app.revanced.patcher.data.BytecodeContext
-import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patches.shared.misc.extension.sharedExtensionPatch
-import app.revanced.patches.line.gms.fingerprints.MainActivityFingerprint
 
-internal val lineMainActivityInitHook = object : BytecodePatch() {
-    override fun execute(context: BytecodeContext) {
-        val mainActivityClass = MainActivityFingerprint.result?.classDef
-            ?: throw Exception("MainActivity not found")
-        
-        mainActivityClass.methods
-            .first { it.name == "onCreate" }
-            .addInstructions(
-                0,
-                "invoke-static {}, Lorg/microg/gms/common/GmsClient;->initialize()V"
-            )
-    }
-}
-
-val extensionPatch = sharedExtensionPatch(
-    extensionName = "LINE Extension",
-    initializationHook = lineMainActivityInitHook
-)
+val extensionPatch = sharedExtensionPatch(homeActivityInitHook)

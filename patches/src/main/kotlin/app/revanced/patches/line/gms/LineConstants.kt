3c27f0e61f89a39b69ca1c6c3a56023acfdb765a
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-09 18:17:18 +0900

    Update LineConstants.kt

--- patches/src/main/kotlin/app/revanced/patches/line/gms/LineConstants.kt
+++ patches/src/main/kotlin/app/revanced/patches/line/gms/LineConstants.kt
@@ -1,9 +1,8 @@
-// LineConstants.kt
 package app.revanced.patches.line.gms
 
 internal object LineConstants {
     const val PACKAGE_NAME = "jp.naver.line.android"
     const val SPOOFED_PACKAGE = "app.revanced.line.android"
-    const val SIGNATURE_SHA256 = "e682fe0bcd60907dfed515e0b8a4de03aa1c281d111a07833986602b6098afd2"
+    const val OFFICIAL_SIGNATURE = "e682fe0bcd60907dfed515e0b8a4de03aa1c281d111a07833986602b6098afd2"
     const val MICROG_SIGNATURE = "1be388ce61a43b6a0b60b42928b6cbe0d83b46a8e873aa193e25f0a589d230c8"
 }

472b99face1f35fa263cbd6b1551a0d4bdcf1494
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-09 23:58:20 +0900

    Update Fingerprints.kt

--- patches/src/main/kotlin/app/revanced/patches/line/misc/gms/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/line/misc/gms/Fingerprints.kt
@@ -3,6 +3,8 @@ package app.revanced.patches.line.misc.gms.fingerprints
 import app.revanced.patcher.fingerprint
 
 internal val MainActivityFingerprint = fingerprint {
+        returns("V")
+    parameters("Landroid/os/Bundle;")
     custom { methodDef, classDef ->
         methodDef.name == "onCreate" && classDef.type == "/MainActivity;"
     }

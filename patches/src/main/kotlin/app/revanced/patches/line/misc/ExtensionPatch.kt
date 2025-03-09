9967a97d5a302be18cf5bab8349e114f256a12d4
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-09 18:48:50 +0900

    Update ExtensionPatch.kt

--- patches/src/main/kotlin/app/revanced/patches/line/misc/ExtensionPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/line/misc/ExtensionPatch.kt
@@ -2,4 +2,7 @@ package app.revanced.patches.line.misc.extension
 
 import app.revanced.patches.shared.misc.extension.sharedExtensionPatch
 
-val extensionPatch = sharedExtensionPatch(homeActivityInitHook)
+val extensionPatch = sharedExtensionPatch(
+    extensionName = "LINE Extension",
+  
+)

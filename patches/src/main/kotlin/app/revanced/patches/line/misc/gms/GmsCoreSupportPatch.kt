c264e2151048c7178f096e3e80fb25443a4d568e
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-09 22:09:53 +0900

    Update GmsCoreSupportPatch.kt

--- patches/src/main/kotlin/app/revanced/patches/line/misc/gms/GmsCoreSupportPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/line/misc/gms/GmsCoreSupportPatch.kt
@@ -4,6 +4,13 @@ import app.revanced.patches.line.misc.extension.extensionPatch
 import app.revanced.patches.shared.misc.gms.gmsCoreSupportPatch
 import app.revanced.patches.line.misc.gms.fingerprints.MainActivityFingerprint
 
+internal object LineConstants {
+    const val PACKAGE_NAME = "jp.naver.line.android"
+    const val SPOOFED_PACKAGE = "app.revanced.line.android"
+    const val OFFICIAL_SIGNATURE = "e682fe0bcd60907dfed515e0b8a4de03aa1c281d111a07833986602b6098afd2"
+    const val MICROG_SIGNATURE = "1be388ce61a43b6a0b60b42928b6cbe0d83b46a8e873aa193e25f0a589d230c8"
+}
+
 @Suppress("unused")
 val gmsCoreSupportPatch = gmsCoreSupportPatch(
     fromPackageName = LineConstants.PACKAGE_NAME,

f3a095703f9a516754567ce846c39aaae939d900
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-09 18:23:33 +0900

    Update build.gradle.kts

--- patches/stub/build.gradle.kts
+++ patches/stub/build.gradle.kts
@@ -1,10 +1,45 @@
 plugins {
-    `java-library`
+    id('com.android.library')
+    id('org.jetbrains.kotlin.android')
 }
 
-description = "Provide Android API stubs for ReVanced Patches."
+android {
+    compileSdkVersion 34
+    buildToolsVersion "34.0.0"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    defaultConfig {
+        minSdkVersion 21
+        targetSdkVersion 34
+    }
+
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_11
+        targetCompatibility JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = '11'
+    }
+}
+
+dependencies {
+    // ReVancedコア依存関係
+    implementation 'app.revanced:patcher:7.0.0'
+    implementation 'app.revanced:library:5.0.0'
+    
+    // Androidサポートライブラリ
+    compileOnly 'com.android.tools.build:gradle:8.1.0'
+    compileOnly 'com.android.tools:common:31.0.0'
+    
+    // Kotlinサポート
+    implementation 'org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.0'
+    
+    // Smali解析
+    implementation 'org.smali:smali:2.5.2'
+    implementation 'org.smali:dexlib2:2.5.2'
+}
+
+repositories {
+    mavenCentral()
+    google()
 }

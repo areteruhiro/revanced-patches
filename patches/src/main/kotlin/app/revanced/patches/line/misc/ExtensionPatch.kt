1639d7e3fc349894bed1c85cdbf63de7fb4d0c5a
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-09 21:27:40 +0900

    Delete patches/src/main/kotlin/app/revanced/patches/line/misc/ExtensionPatch.kt

--- patches/src/main/kotlin/app/revanced/patches/line/misc/ExtensionPatch.kt
+++
@@ -1,6 +0,0 @@
-package app.revanced.patches.line.misc.extension
-
-import app.revanced.patches.line.misc.extension.hooks.startActivityInitHook
-import app.revanced.patches.shared.misc.extension.sharedExtensionPatch
-
-val extensionPatch = sharedExtensionPatch(startActivityInitHook)

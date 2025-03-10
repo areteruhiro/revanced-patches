df008c0fd0e0dc777208606e67930f3437ea5b3d
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-10 22:43:00 +0900

    Update and rename StartActivityInitHook.kt to Hooks.kt

--- patches/src/main/kotlin/app/revanced/patches/line/misc/extension/hooks/StartActivityInitHook.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.patches.line.misc.extension.hooks
-
-import app.revanced.patches.shared.misc.extension.extensionHook
-
-internal val applicationInitHook = extensionHook {
-    custom { methodDef, classDef ->
-           methodDef.name == "onCreate" && classDef.type == "Ljp/naver/line/android/activity/main/MainActivity;"
-    }
-}

e6efb8f7f52334bca56ebcfae9dfde7100bfd4eb
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-10 18:21:29 +0900

    Update Fingerprints.kt

--- patches/src/main/kotlin/app/revanced/patches/line/misc/gms/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/line/misc/gms/Fingerprints.kt
@@ -1,15 +1,9 @@
 package app.revanced.patches.line.misc.gms.fingerprints
 
 import app.revanced.patcher.fingerprint
-import app.revanced.patcher.Fingerprint
 
 internal val MainActivityFingerprint = fingerprint {
     custom { methodDef, classDef ->
-        val isMatch = methodDef.name == "onCreate" && classDef.type == "Ljp/naver/line/android/activity/main/MainActivity;"
-        if (!isMatch) {
-            println("Fingerprint mismatch: methodName=${methodDef.name}, classType=${classDef.type}")
-        }
-
-        isMatch
+        methodDef.name == "onCreate" && classDef.type == "Ljp/naver/line/android/activity/main/MainActivity;"
     }
 }

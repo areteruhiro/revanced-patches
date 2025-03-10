d304299d620ea6b06e14424a9f6d2dd91813efa7
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-10 23:52:18 +0900

    Update Fingerprints.kt

--- patches/src/main/kotlin/app/revanced/patches/shared/misc/gms/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/shared/misc/gms/Fingerprints.kt
@@ -5,11 +5,7 @@ import com.android.tools.smali.dexlib2.AccessFlags
 
 const val GET_GMS_CORE_VENDOR_GROUP_ID_METHOD_NAME = "getGmsCoreVendorGroupId"
 
-internal val gmsCoreSupportFingerprint = fingerprint {
-    custom { _, classDef ->
-        classDef.endsWith("GmsCoreSupport;")
-    }
-}
+
 
 internal val googlePlayUtilityFingerprint = fingerprint {
     accessFlags(AccessFlags.PUBLIC, AccessFlags.STATIC)

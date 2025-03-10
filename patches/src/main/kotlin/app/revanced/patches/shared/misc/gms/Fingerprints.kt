9d8c512a0ae9d1797c16567c2a86d5c9e5b4d7f0
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-10 18:13:21 +0900

    Update Fingerprints.kt

--- patches/src/main/kotlin/app/revanced/patches/shared/misc/gms/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/shared/misc/gms/Fingerprints.kt
@@ -13,12 +13,18 @@ internal val gmsCoreSupportFingerprint = fingerprint {
 
 internal val googlePlayUtilityFingerprint = fingerprint {
     accessFlags(AccessFlags.PUBLIC, AccessFlags.STATIC)
+    returns("I")
+    parameters("L", "I")
     strings(
+        "This should never happen.",
+        "MetadataValueReader",
         "com.google.android.gms",
     )
 }
 
 internal val serviceCheckFingerprint = fingerprint {
     accessFlags(AccessFlags.PUBLIC, AccessFlags.STATIC)
+    returns("V")
+    parameters("L", "I")
     strings("Google Play Services not available")
 }

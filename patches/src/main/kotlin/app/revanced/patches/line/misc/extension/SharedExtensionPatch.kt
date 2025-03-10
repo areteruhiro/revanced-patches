190a0768cf96872bae88ed6369eb4e049b569688
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-10 22:45:14 +0900

    Delete patches/src/main/kotlin/app/revanced/patches/line/misc/extension/SharedExtensionPatch.kt

--- patches/src/main/kotlin/app/revanced/patches/line/misc/extension/SharedExtensionPatch.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.patches.line.misc.extension
-
-import app.revanced.patches.line.misc.extension.hooks.applicationInitHook
-import app.revanced.patches.shared.misc.extension.sharedExtensionPatch
-
-val sharedExtensionPatch = sharedExtensionPatch(
-    "line",
-    applicationInitHook,
-)

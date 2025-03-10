982ff9fbe8cb3526f7e96985d3de3a6cbcb115e6
areteruhiro <108941410+areteruhiro@users.noreply.github.com>
2025-03-10 18:31:18 +0900

    Update ExtensionPatch.kt

--- patches/src/main/kotlin/app/revanced/patches/line/misc/extension/ExtensionPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/line/misc/extension/ExtensionPatch.kt
@@ -3,4 +3,4 @@ package app.revanced.patches.line.misc.extension
 import app.revanced.patches.line.misc.extension.hooks.startActivityInitHook
 import app.revanced.patches.shared.misc.extension.sharedExtensionPatch
 
-val extensionPatch = sharedExtensionPatch(startActivityInitHook)
+val extensionPatch = sharedExtensionPatchh(startActivityInitHook)
